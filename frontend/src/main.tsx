7d3171275fdb7e2e47371a71f4aae7336d2cd803
alexy4744 <alexy4744@gmail.com>
2023-09-18 22:33:27 -0400

    add chakra ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App.tsx";
+
+import "./index.css";
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
