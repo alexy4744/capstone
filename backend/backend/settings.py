849e9b5f95a1152f980c2ab3768b48552dace77a
michniks2001 <57420719+michniks2001@users.noreply.github.com>
2023-09-27 16:39:51 -0400

    Add files via upload

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': BASE_DIR / 'Students',
+        'ENFORCE_SCHEMA': False,
     }
 }
 
