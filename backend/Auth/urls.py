0431de283eec5975661d3236224ced420af9851a
michniks2001 <57420719+michniks2001@users.noreply.github.com>
2023-11-02 16:42:44 -0400

    Delete backend/Auth directory

--- backend/Auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-#URL Configuration
-urlpatterns = [
-    path('hi/', views.sayHi),
-    path('api/register/', views.UserRegistrationView.as_view(), name='register')
-]
