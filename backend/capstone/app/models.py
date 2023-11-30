9a3898a302692c3775e3df849d10b429ddceffb3
michniks2001 <57420719+michniks2001@users.noreply.github.com>
2023-11-30 17:40:11 -0500

    added UserResponse model

--- backend/capstone/app/models.py
+++ backend/capstone/app/models.py
@@ -15,3 +15,14 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.CharField(max_length=4)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserResponse(models.Model):
+    user_id = models.CharField(max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    submitted_answer = models.CharField(max_length=4)
+    is_correct = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.submitted_answer}'
