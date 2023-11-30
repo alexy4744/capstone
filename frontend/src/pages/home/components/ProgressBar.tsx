ebe2224b1c41c5c1ba905ee3222f6ca08e0d07b7
Cheuk Lam Cheung <ccheung792423@gmail.com>
2023-11-30 17:14:33 -0500

    change home page to use exam date from settings

--- frontend/src/pages/home/components/ProgressBar.tsx
+++ frontend/src/pages/home/components/ProgressBar.tsx
@@ -1,18 +1,16 @@
-import { Flex, Box, Text} from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import Countdown, { CountdownRendererFn } from "react-countdown";
 
-export const ProgressBar = ({ initialDate }: { initialDate?: number }) => {
-    const [progressPercent, setProgessPercent] = useState<number>(50);
-
-    const testDate = Date.parse("04 Dec 2024 01:00:00 EST");
+export const ProgressBar = ({ initialDate, testDate }: { initialDate?: number, testDate?: Date }) => {
+    const [progressPercent, setProgessPercent] = useState<number>(30);
 
     useEffect(() => {
-        if (initialDate) {
-            const initialDifference = testDate - initialDate;
-            const currentDifference = testDate - Date.now();
+        if (initialDate && testDate) {
+            const initialDifference = testDate.getTime() - initialDate;
+            const currentDifference = testDate.getTime() - Date.now();
             if (initialDifference > 0 && currentDifference > 0) {
-                setProgessPercent((currentDifference / initialDifference)*100);
+                setProgessPercent((1 - (currentDifference / initialDifference)) * 100);
             }
         }
     }, [initialDate, testDate])
@@ -21,9 +19,14 @@ export const ProgressBar = ({ initialDate }: { initialDate?: number }) => {
         if (completed) {
             return <Text>Exam is over!</Text>
         } else {
+            if (!initialDate) {
+                if (70 - days > 0) {
+                    setProgessPercent(100 - days);
+                }
+            }
             if (days > 31) {
-                const timeRemaining = Math.round(days / 30.4167);
-                return <Text>{timeRemaining} month{timeRemaining === 1 ? "" : "s"} left</Text>
+                const monthsRemaining = Math.round(days / 30.4167);
+                return <Text>{monthsRemaining} month{monthsRemaining === 1 ? "" : "s"} left</Text>
             } else if (days > 0) {
                 return <Text>{days} day{days === 1 ? "" : "s"} left</Text>
             } else if (hours > 0) {
@@ -36,9 +39,11 @@ export const ProgressBar = ({ initialDate }: { initialDate?: number }) => {
 
     return (
         <Box w="100%" px="5%">
-            <Flex justifyContent={progressPercent < 50 ? "flex-end" : "flex-start"}>
-                <Countdown date={testDate} renderer={renderer} />
-            </Flex>
+            {testDate && (
+                <Flex justifyContent={progressPercent < 50 ? "flex-end" : "flex-start"}>
+                    <Countdown date={testDate} renderer={renderer} />
+                </Flex>
+            )}
             <Box
                 position="relative"
                 w="100%"
