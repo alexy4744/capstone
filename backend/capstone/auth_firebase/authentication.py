d3d856454c026507306d5e84f07119b3890bf078
michniks2001 <57420719+michniks2001@users.noreply.github.com>
2023-12-05 17:47:35 -0500

    Create authentication.py

---
+++ backend/capstone/auth_firebase/authentication.py
@@ -0,0 +1,35 @@
+from firebase_admin import auth, credentials
+from django.contrib.auth.models import User
+from django.utils import timezone
+from rest_framework import authentications, exceptions
+from . exceptions import FirebaseError, InvalidAuthToken, NoAuthToken
+
+cred = credentials.Certificate("creds.json")
+firebase_admin.initialize_app(cred)
+
+class FirebaseAuthentication(authentication.BaseAuthentication):
+  def authenticate(self, request):
+    auth_header = request.META.get("HTTP_AUTHORIZATION")
+    if not auth_header:
+      raise NoAuthToken("No auth token provided")
+
+    id_token = auth_header.split().pop()
+    decoded_token = None
+
+    try:
+      decoded_token = auth.verify_id_token(id_token)
+    except Exception:
+      raise InvalidAuthToken("Invalid auth token")
+
+    if not id_token or not decoded_token:
+      return None
+
+    try:
+      uid = decoded_token.get('uid')
+    except:
+      raise FirebaseError()
+
+    user, created = User.objects.get_or_create(username=uid)
+    user.profile.last_activity = timezone.localtime()
+
+    return user, None
