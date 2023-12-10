77aa9fdc1427d82b1d4df59ed954505feab89002
alexy4744 <alexy4744@gmail.com>
2023-12-09 22:45:11 -0500

    fix CORS origin header on backend

--- backend/capstone/capstone/settings.py
+++ backend/capstone/capstone/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,11 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4173',
+    'http://localhost:5173',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
