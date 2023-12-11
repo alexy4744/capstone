e78170f9624ef2c842cf7f1c6d73e1ea308a44cc
alexy4744 <alexy4744@gmail.com>
2023-12-11 00:25:29 -0500

    use base url in react router dom

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -8,44 +8,49 @@ import HomePage from "../pages/home/routes/Home";
 import AnswerPage from "../pages/questions/routes/AnswerPage";
 import { Settings } from "../pages/settings";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      children: [
+        {
+          element: <RequireNoAuth />,
+          children: [
+            {
+              path: "forgot-password",
+              element: <ForgotPassword />,
+            },
+            {
+              path: "login",
+              element: <Login />,
+            },
+            {
+              path: "register",
+              element: <Register />,
+            },
+          ],
+        },
+        {
+          element: <RequireAuth />,
+          children: [
+            {
+              path: "",
+              element: <HomePage />,
+            },
+            {
+              path: "questions/",
+              element: <AnswerPage />,
+            },
+            {
+              path: "settings",
+              element: <Settings />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    children: [
-      {
-        element: <RequireNoAuth />,
-        children: [
-          {
-            path: "forgot-password",
-            element: <ForgotPassword />,
-          },
-          {
-            path: "login",
-            element: <Login />,
-          },
-          {
-            path: "register",
-            element: <Register />,
-          },
-        ],
-      },
-      {
-        element: <RequireAuth />,
-        children: [
-          {
-            path: "",
-            element: <HomePage />
-          },
-          {
-            path: "questions/",
-            element: <AnswerPage />
-          },
-          {
-            path: "settings",
-            element: <Settings />,
-          }
-        ],
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
