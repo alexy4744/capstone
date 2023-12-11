67dcad68667180d8c055590742a1e780ba9a3d57
alexy4744 <alexy4744@gmail.com>
2023-12-10 22:43:06 -0500

    fix type error in answer page

--- frontend/src/pages/questions/routes/AnswerPage.tsx
+++ frontend/src/pages/questions/routes/AnswerPage.tsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import {
-    Badge,
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    Box,
-    Flex,
-    Text,
-    Image,
-    Button,
-    AbsoluteCenter,
-    HStack,
-    Input,
-    Spinner,
+  Badge,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Box,
+  Flex,
+  Text,
+  Image,
+  Button,
+  AbsoluteCenter,
+  HStack,
+  Input,
+  Spinner,
 } from "@chakra-ui/react";
 
 import { MdKeyboardArrowRight } from "react-icons/md";
@@ -34,218 +34,221 @@ import { DefaultLayout } from "../../../layout/DefaultLayout";
 import { TopicDictionary } from "../../../components/CategorySelector";
 
 const mapDifficultyLevelToText = (difficulty: number) => {
-    switch (difficulty) {
-        case 1:
-            return "easy";
-        case 2:
-            return "normal";
-        case 3:
-            return "hard";
-        default:
-            throw new RangeError("Difficulty level must be between 1 and 3.");
-    }
+  switch (difficulty) {
+    case 1:
+      return "easy";
+    case 2:
+      return "normal";
+    case 3:
+      return "hard";
+    default:
+      throw new RangeError("Difficulty level must be between 1 and 3.");
+  }
 };
 
 const AnswerPage = () => {
-    const [queryParameters] = useSearchParams();
-    const [question, setQuestion] = useState<Question | null | undefined>(null);
-    const navigate = useNavigate();
+  const [queryParameters] = useSearchParams();
+  const [question, setQuestion] = useState<Question | null | undefined>(null);
+  const navigate = useNavigate();
 
-    const [calculatorOn, setCalculatorOn] = useState<boolean>(false);
-    const [drawerStatus, setDrawerStatus] = useState<DrawerStatus>("stop");
-    const [drawingOn, setDrawingOn] = useState<boolean>(false);
-    const [eraserOn, setEraserOn] = useState<boolean>(false);
-    const [referenceOn, setReferenceOn] = useState<boolean>(false);
-    const [selectedColor, setSelectedColor] = useState<string>("");
-    const handleDrawing = (color: string = selectedColor, enable: boolean = !drawingOn) => {
-        if (color === "") {
-            color = "black";
-        }
-        setSelectedColor(color);
-        setDrawingOn(enable);
-        setEraserOn(false);
-    };
-    const handleSwitchPenTool = () => {
-        setEraserOn(!eraserOn);
-        setDrawingOn(true);
-    };
-    const handleCalculatorToolClick = () => {
-        setDrawerStatus(!calculatorOn ? "active" : "stop");
-        setReferenceOn(false);
-        setCalculatorOn(!calculatorOn);
-    };
-    const handleReferenceToolClick = () => {
-        setDrawerStatus(!referenceOn ? "active" : "stop");
-        setCalculatorOn(false);
-        setReferenceOn(!referenceOn);
-    };
+  const [calculatorOn, setCalculatorOn] = useState<boolean>(false);
+  const [drawerStatus, setDrawerStatus] = useState<DrawerStatus>("stop");
+  const [drawingOn, setDrawingOn] = useState<boolean>(false);
+  const [eraserOn, setEraserOn] = useState<boolean>(false);
+  const [referenceOn, setReferenceOn] = useState<boolean>(false);
+  const [selectedColor, setSelectedColor] = useState<string>("");
+  const handleDrawing = (color: string = selectedColor, enable: boolean = !drawingOn) => {
+    if (color === "") {
+      color = "black";
+    }
+    setSelectedColor(color);
+    setDrawingOn(enable);
+    setEraserOn(false);
+  };
+  const handleSwitchPenTool = () => {
+    setEraserOn(!eraserOn);
+    setDrawingOn(true);
+  };
+  const handleCalculatorToolClick = () => {
+    setDrawerStatus(!calculatorOn ? "active" : "stop");
+    setReferenceOn(false);
+    setCalculatorOn(!calculatorOn);
+  };
+  const handleReferenceToolClick = () => {
+    setDrawerStatus(!referenceOn ? "active" : "stop");
+    setCalculatorOn(false);
+    setReferenceOn(!referenceOn);
+  };
 
-    useEffect(() => {
-        const id = queryParameters.get("id");
-        if (id && !isNaN(id)) {
-            getQuestion(id).then((question) => setQuestion(question));
-        } else {
-            const calculator = queryParameters.get("calculator");
-            const difficulty = queryParameters.get("difficulty");
-            const topic = queryParameters.get("topic");
-            if (!calculator && !difficulty && !topic) {
-                setQuestion(undefined);
-            } else {
-                getQuestions().then((allQuestions) => {
-                    const filteredList = allQuestions
-                        .filter((q) =>
-                            (calculator ? `${q.calculator}` === calculator : true)
-                            && (difficulty ? mapDifficultyLevelToText(q.difficulty) === difficulty : true)
-                            && (topic ? q.category === topic.toUpperCase() : true));
-                    if (filteredList.length === 0) {
-                        setQuestion(undefined);
-                    } else {
-                        const selectedQuestion = filteredList[Math.floor(Math.random() * filteredList.length)]
-                        setQuestion(selectedQuestion);
-                    }
-                });
-            }
-        }
-    }, [queryParameters]);
+  useEffect(() => {
+    const id = queryParameters.get("id");
+    if (id) {
+      getQuestion(id).then((question) => setQuestion(question));
+    } else {
+      const calculator = queryParameters.get("calculator");
+      const difficulty = queryParameters.get("difficulty");
+      const topic = queryParameters.get("topic");
+      if (!calculator && !difficulty && !topic) {
+        setQuestion(undefined);
+      } else {
+        getQuestions().then((allQuestions) => {
+          const filteredList = allQuestions.filter(
+            (q) =>
+              (calculator ? `${q.calculator}` === calculator : true) &&
+              (difficulty ? mapDifficultyLevelToText(q.difficulty) === difficulty : true) &&
+              (topic ? q.category === topic.toUpperCase() : true)
+          );
+          if (filteredList.length === 0) {
+            setQuestion(undefined);
+          } else {
+            const selectedQuestion = filteredList[Math.floor(Math.random() * filteredList.length)];
+            setQuestion(selectedQuestion);
+          }
+        });
+      }
+    }
+  }, [queryParameters]);
 
-    if (question === null) {
-        return (
-            <DefaultLayout>
-                <Spinner position="absolute" top="50%" left="50%" />
-            </DefaultLayout>
-        );
-    } else if (question === undefined) {
-        return (
-            <DefaultLayout>
-                <AbsoluteCenter textAlign="center">
-                    <FaPersonCircleQuestion size="100" />
-                    <Text my="10">No questions found</Text>
-                    <Button onClick={() => navigate(-1)} >
-                        Go Back
-                    </Button>
-                </AbsoluteCenter>
-            </DefaultLayout>
-        );
+  if (question === null) {
+    return (
+      <DefaultLayout>
+        <Spinner position="absolute" top="50%" left="50%" />
+      </DefaultLayout>
+    );
+  } else if (question === undefined) {
+    return (
+      <DefaultLayout>
+        <AbsoluteCenter textAlign="center">
+          <FaPersonCircleQuestion size="100" />
+          <Text my="10">No questions found</Text>
+          <Button onClick={() => navigate(-1)}>Go Back</Button>
+        </AbsoluteCenter>
+      </DefaultLayout>
+    );
+  }
+
+  const difficulty = mapDifficultyLevelToText(question.difficulty);
+
+  const handleAnswerSubmission = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!question) {
+      throw new Error("Missing question ID from URL.");
     }
 
-    const difficulty = mapDifficultyLevelToText(question.difficulty);
+    const form = event.currentTarget;
 
-    const handleAnswerSubmission = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const answer = form.answer.value;
 
-        if (!question) {
-            throw new Error("Missing question ID from URL.");
-        }
+    if (!answer) {
+      throw new Error("Answer cannot be empty.");
+    }
 
-        const form = event.currentTarget;
+    const submission = await answerQuestion(question.id, answer);
 
-        const answer = form.answer.value;
+    if (!submission.submitted_answer) {
+      return alert("Incorrect answer. Please try again.");
+    }
 
-        if (!answer) {
-            throw new Error("Answer cannot be empty.");
-        }
+    alert("Correct answer!");
+  };
 
-        const submission = await answerQuestion(question.id, answer);
+  return (
+    <DefaultLayout backgroundColor="gray.50">
+      <Breadcrumb
+        spacing="8px"
+        separator={<MdKeyboardArrowRight />}
+        position="relative"
+        bg="#F5FBFF"
+        px="10"
+        py="2"
+      >
+        <BreadcrumbItem zIndex="9999">
+          <BreadcrumbLink href="#">SAT</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem zIndex="9999">
+          <BreadcrumbLink href="#">
+            {question && !question.calculator && "No "}Calculator Allowed
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem zIndex="9999">
+          <BreadcrumbLink href="#">
+            {question && question.category && TopicDictionary[question.category]}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Flex w="100%" px="10" py="5">
+        <Workspace {...{ drawingOn, eraserOn, selectedColor }}>
+          <Box>
+            <Image src={question.image} alt={question.title} maxHeight="100vh" />
 
-        if (!submission.submitted_answer) {
-            return alert("Incorrect answer. Please try again.");
-        }
+            <form onSubmit={handleAnswerSubmission} style={{ display: "flex" }}>
+              <HStack bgColor="white" position="relative" zIndex={9999}>
+                {question.multiple_choice ? (
+                  <Select
+                    options={["A", "B", "C", "D"].map((choice) => ({
+                      label: choice,
+                      value: choice.toLowerCase(),
+                    }))}
+                    isRequired
+                    isSearchable={false}
+                    placeholder="Select an answer"
+                    name="answer"
+                  />
+                ) : (
+                  <Input type="text" name="answer" />
+                )}
 
-        alert("Correct answer!");
-    };
+                <Box>
+                  <Input type="submit" value="Submit" />
+                </Box>
+              </HStack>
+            </form>
+          </Box>
+        </Workspace>
 
-    return (
-        <DefaultLayout backgroundColor="gray.50">
-            <Breadcrumb
-                spacing="8px"
-                separator={<MdKeyboardArrowRight />}
-                position="relative"
-                bg="#F5FBFF"
-                px="10"
-                py="2"
-            >
-                <BreadcrumbItem zIndex="9999">
-                    <BreadcrumbLink href="#">SAT</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbItem zIndex="9999">
-                    <BreadcrumbLink href="#">{question && !question.calculator && "No "}Calculator Allowed</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbItem zIndex="9999">
-                    <BreadcrumbLink href="#">{question && question.category && TopicDictionary[question.category]}</BreadcrumbLink>
-                </BreadcrumbItem>
-            </Breadcrumb>
-            <Flex w="100%" px="10" py="5">
-                <Workspace {...{ drawingOn, eraserOn, selectedColor }}>
-                    <Box>
-                        <Image src={question.image} alt={question.title} maxHeight="100vh"/>
+        {/* Toolbar */}
+        <Flex flex="1" flexDirection="column" alignItems="flex-end" py="5">
+          <Badge
+            colorScheme={difficulty.toLowerCase()}
+            fontSize={["sm", "sm", "md", "md"]}
+            px="3"
+            zIndex="1"
+          >
+            {difficulty}
+          </Badge>
 
-                        <form onSubmit={handleAnswerSubmission} style={{ display: "flex" }}>
-                            <HStack bgColor="white" position="relative" zIndex={9999}>
-                                {question.multiple_choice ? (
-                                    <Select
-                                        options={["A", "B", "C", "D"].map((choice) => ({
-                                            label: choice,
-                                            value: choice.toLowerCase(),
-                                        }))}
-                                        isRequired
-                                        isSearchable={false}
-                                        placeholder="Select an answer"
-                                        name="answer"
-                                    />
-                                ) : (
-                                    <Input type="text" name="answer" />
-                                )}
+          <Box py="5">
+            <Text textAlign="center">Time Passed:</Text>
+            <Timer />
+          </Box>
 
-                                <Box>
-                                    <Input type="submit" value="Submit" />
-                                </Box>
-                            </HStack>
-                        </form>
-                    </Box>
-                </Workspace>
+          <Box>
+            <Text pb="2">Tools:</Text>
 
-                {/* Toolbar */}
-                <Flex flex="1" flexDirection="column" alignItems="flex-end" py="5">
-                    <Badge
-                        colorScheme={difficulty.toLowerCase()}
-                        fontSize={["sm", "sm", "md", "md"]}
-                        px="3"
-                        zIndex="1"
-                    >
-                        {difficulty}
-                    </Badge>
+            <DrawingToolbar
+              {...{ drawingOn, eraserOn, selectedColor, handleDrawing, handleSwitchPenTool }}
+            />
 
-                    <Box py="5">
-                        <Text textAlign="center">Time Passed:</Text>
-                        <Timer />
-                    </Box>
+            <Box mt="60px" py="5">
+              <ReferenceTool {...{ referenceOn, handleReferenceToolClick }} />
+            </Box>
 
-                    <Box>
-                        <Text pb="2">Tools:</Text>
+            {question.calculator && (
+              <Box py="5">
+                <CalculatorTool {...{ calculatorOn, handleCalculatorToolClick }} />
+              </Box>
+            )}
+          </Box>
+        </Flex>
 
-                        <DrawingToolbar
-                            {...{ drawingOn, eraserOn, selectedColor, handleDrawing, handleSwitchPenTool }}
-                        />
-
-                        <Box mt="60px" py="5">
-                            <ReferenceTool {...{ referenceOn, handleReferenceToolClick }} />
-                        </Box>
-
-                        {question.calculator && (
-                            <Box py="5">
-                                <CalculatorTool {...{ calculatorOn, handleCalculatorToolClick }} />
-                            </Box>
-                        )}
-                    </Box>
-                </Flex>
-
-                <ToolDrawer
-                    {...{ drawerStatus, calculatorOn, referenceOn }}
-                    handleToggle={() => setDrawerStatus(drawerStatus === "active" ? "inactive" : "active")}
-                />
-            </Flex>
-        </DefaultLayout>
-    );
+        <ToolDrawer
+          {...{ drawerStatus, calculatorOn, referenceOn }}
+          handleToggle={() => setDrawerStatus(drawerStatus === "active" ? "inactive" : "active")}
+        />
+      </Flex>
+    </DefaultLayout>
+  );
 };
 
 export default AnswerPage;
