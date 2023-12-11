043b7646f8e6c7646363d9a09156e4a655a998ec
alexy4744 <alexy4744@gmail.com>
2023-12-10 20:09:25 -0500

    start timer from 0

--- frontend/src/pages/questions/components/Timer.tsx
+++ frontend/src/pages/questions/components/Timer.tsx
@@ -3,29 +3,29 @@ import { usePreferences } from "../../../hooks";
 import { useState, useEffect } from "react";
 
 export const Timer = () => {
-    const [time, setTime] = useState<number>(11890);
-    const { preferences } = usePreferences();
+  const [time, setTime] = useState<number>(0);
+  const { preferences } = usePreferences();
 
-    useEffect(() => {
-        const intervalId = setInterval(() => setTime(time + 1), 10);
-        return () => clearInterval(intervalId);
-    })
+  useEffect(() => {
+    const intervalId = setInterval(() => setTime(time + 1), 10);
+    return () => clearInterval(intervalId);
+  });
 
-    const hours = Math.floor(time / 360000);
-    const minutes = Math.floor((time % 360000) / 6000);
-    const seconds = Math.floor((time % 6000) / 100);
+  const hours = Math.floor(time / 360000);
+  const minutes = Math.floor((time % 360000) / 6000);
+  const seconds = Math.floor((time % 6000) / 100);
 
-    return (
-        <Flex justifyContent="center">
-            <Text
-                fontWeight="bold"
-                fontSize={["md", "lg", "xl", "xl"]}
-                color={(preferences.timerRed && minutes > 1) ? "red.500" : "initial"}
-                transition="color 0.3s">
-                {hours > 0 && `${hours}:`}
-                {minutes.toString().padStart(2, "0")}:
-                {seconds.toString().padStart(2, "0")}
-            </Text>
-        </Flex>
-    );
+  return (
+    <Flex justifyContent="center">
+      <Text
+        fontWeight="bold"
+        fontSize={["md", "lg", "xl", "xl"]}
+        color={preferences.timerRed && minutes > 1 ? "red.500" : "initial"}
+        transition="color 0.3s"
+      >
+        {hours > 0 && `${hours}:`}
+        {minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}
+      </Text>
+    </Flex>
+  );
 };
